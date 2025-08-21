b75ba0e49b3b1f9e117a63976b60e7f8e8b78bed
Muthamizh2003 <muthamizhcoding@gmail.com>
2025-08-21 09:40:06 +0530

    want to add all now

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/portfolio-react/",  // 👈 correct spelling
+  base: "/my-portfolio-2003/",  // 👈 correct spelling
   plugins: [react()],
 })
